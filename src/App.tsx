609e24df7e7ff14ebb2f6fd3b8bc858c9b336a76
suraj <ridesuraj@gmail.com>
2025-08-24 15:15:47 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Resume } from './components/Resume';
 export function App() {
-  return <div className="flex w-full min-h-screen bg-black">
+  return <div className="flex w-full min-h-screen bg-main">
       <Resume />
     </div>;
 }
