7cfc1cf65a57f99d7595f4ea037585e7639107e9
suraj <ridesuraj@gmail.com>
2025-08-24 15:17:38 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,4 @@
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary-color': 'var(--primary-color)',
-        'primary-hover': 'var(--primary-hover)',
-        'bg-main': 'var(--bg-main)',
-        'bg-card': 'var(--bg-card)',
-        'bg-btn': 'var(--bg-button)',
-        'bg-btn-hover': 'var(--bg-button-hover)',
-        'text-primary': 'var(--text-primary)',
-        'text-secondary': 'var(--text-secondary)',
-        'text-muted': 'var(--text-muted)',
-        'border-custom': 'var(--border-color)'
-      }
-    }
-  }
-}
+export default {content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],}
